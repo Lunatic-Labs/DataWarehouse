f0cd5ddc4d253622850be099d68f218c627c586d
jdkingsbury <jerry.daniel.kingsbury@gmail.com>
2022-10-10 16:45:17 -0500

    Commiting for help

--- datawarehouse/service/check_message.py
+++ datawarehouse/service/check_message.py
@@ -1,22 +1,20 @@
 import sqlalchemy
 import json
 
-with open('sample.json') as f:
-        data = json.load(f)
+with open("sample.json") as f:
+    data = json.load(f)
 
 
 class checkJsonFile:
-
     @classmethod
     def check_dictionaries(self, data, val):
         # found = False
 
-        sources = data['sources']
-        
+        sources = data["sources"]
+
         for i in sources:
-            found = sources.get(i)
+            found = sources.get(val)
             print(found)
-                
 
     @classmethod
     def required_fields(self, data):
@@ -26,34 +24,30 @@ class checkJsonFile:
         req_metric_layer = ["data_type", "name"]
 
         for i in req_outer_layer:
-            if(not data.get(i)):
+            if not data.get(i):
                 print("Missing required field:", i)
 
         for i in req_source_layer:
-            if(not self.check_dictionaries(data["sources"], i)):
+            if not self.check_dictionaries(data["sources"], i):
                 print("Missing required field:", i)
-
-    
-        # if not data.get("metrics"):
-        #     print("Key does not exist")
 
 
 checkJsonFile.required_fields(data)
 
 
-    # Outermost Layer
-        # group_name    string      required        Title of the group
-        # sources       string      optional        Type of the data taken in
-        # location      string      optional        Where the data is sourced from
-        # sources       array       required        an array of sources (Json object)
+# Outermost Layer
+# group_name    string      required        Title of the group
+# sources       string      optional        Type of the data taken in
+# location      string      optional        Where the data is sourced from
+# sources       array       required        an array of sources (Json object)
 
-    # Source Layer
-        # metrics       array       required        an array of metrics (Json object)
-        # name          string      required        the name of the source
-        # tz.info       string      optional        timezone
+# Source Layer
+# metrics       array       required        an array of metrics (Json object)
+# name          string      required        the name of the source
+# tz.info       string      optional        timezone
 
-    # Metric Layer
-        # asc           boolean     optional        Default sort direction
-        # data_type     string      REQUIRED        MUST BE A SUPPORTED DATA_TYPE
-        # name          string      required        name of the metric (column)
-        # units         string      optional        units of measurement
+# Metric Layer
+# asc           boolean     optional        Default sort direction
+# data_type     string      REQUIRED        MUST BE A SUPPORTED DATA_TYPE
+# name          string      required        name of the metric (column)
+# units         string      optional        units of measurement

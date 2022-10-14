111ff78927be336b705c4597b1410e890e8bfe7a
nmullins417 <nmullins417@gmail.com>
2022-10-14 09:58:48 -0500

    First commit with Black code formatter installed

--- datawarehouse/controller/api/html_handshake/__init__.py
+++ datawarehouse/controller/api/html_handshake/__init__.py
@@ -1,16 +1,21 @@
 from flask import Blueprint, request, render_template
 
-hs_bp = Blueprint("hs_bp", __name__, url_prefix="/handshake", template_folder='templates')
+hs_bp = Blueprint(
+    "hs_bp", __name__, url_prefix="/handshake", template_folder="templates"
+)
 
-@hs_bp.route('/', methods=['GET', 'POST'])
+
+@hs_bp.route("/", methods=["GET", "POST"])
 def hs_func():
     if request.method == "POST":
-       name = request.form.get("name")
-       number = request.form.get("number")
-       return "Name: %s; Number: %s" % (name, number)
+        name = request.form.get("name")
+        number = request.form.get("number")
+        return "Name: %s; Number: %s" % (name, number)
     return render_template("handshake.html")
 
-if __name__ == '__main__':
-   hs_bp.run()
 
-   #asdklfjasld;kfj;askdjf;laskdjf asdasdf
+if __name__ == "__main__":
+    hs_bp.run()
+
+    # asdklfjasld;kfj;askdjf;laskdjf asdasdf
+

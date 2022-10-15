9720f49147f32d454eb4a350f22e5572360fd061
Taylor Hudson <taylorhudson42@gmail.com>
2022-10-15 15:29:18 -0500

    multiple column support, minor changes.

--- datawarehouse/service/data_insert_service.py
+++ datawarehouse/service/data_insert_service.py
@@ -1,46 +1,56 @@
 import sqlalchemy
+from datawarehouse.service import BaseService
+
+from datetime import datetime
+from datawarehouse.config.db import config as db
+
+from datawarehouse.model import group, source, metric
 
 
-class InsertDataService:
-    def __init__(self, url, source_uid=None, metric_uid=None, data=None):
-        """
-        TODO: Connect using our existing connections.
-        """
-        self._engine = sqlalchemy.create_engine(url)
-        self._connection = self._engine.connect()
-        self._metadata_obj = sqlalchemy.MetaData()
-        self._source_uid = source_uid
-        self._metric_uid = metric_uid
-        self.data = data
+class InsertDataService(BaseService):
+    _engine = db.engine
+    _connection = _engine.connect()
+    _metadata_obj = db.meta
+
+    # def __init__(self, url, source_uid=None, metric_uid=None, data=None):
+    #     """
+    #     TODO: Connect using our existing connections.
+    #     """
+    #     self._engine = db.engine
+    #     self._connection = self._engine.connect()
+    #     self._metadata_obj = db.meta
 
     """
     TODO: Change this from reading a txt file to taking JSON.
     """
 
-    def getData(self, file):
-        with open(file, "r") as f:
-            self._source_uid = f.readline()
-            self._metric_uid = f.readline()
-            self.data = f.readline()
-            # Getting rid of the newline characters.
-            self._source_uid = self._source_uid.rstrip()
-            self._metric_uid = self._metric_uid.rstrip()
-            self.data = self.data.rstrip()
+    # def getData(self, file):
+    #     with open(file, "r") as f:
+    #         self._source_uid = f.readline()
+    #         self._metric_uid = f.readline()
+    #         self.data = f.readline()
+    #         # Getting rid of the newline characters.
+    #         self._source_uid = self._source_uid.rstrip()
+    #         self._metric_uid = self._metric_uid.rstrip()
+    #         self.data = self.data.rstrip()
 
-    def _verifyInformation(self):
-        metric_table = sqlalchemy.Table(
+    def _verifyInformation(self, data):
+        metric = sqlalchemy.Table(
             "metric", self._metadata_obj, autoload=True, autoload_with=self._engine
         )
-        stmt = sqlalchemy.select([metric_table]).where(
-            metric_table.columns.metric_uid == self._metric_uid
-        )
-        result = self._connection.execute(stmt).fetchone()
+        for metric_ in data["metrics"]:
+            stmt = sqlalchemy.select([metric]).where(
+                metric.columns.metric_uid == metric_["metric_uid"]
+            )
+            result = self._connection.execute(stmt).fetchone()
 
-        if result is None:
-            raise Exception("metric_uid was not found.")
+            if result is None:
+                raise Exception("metric_uid was not found.")
 
-        if result["source_uid"] != self._source_uid:
-            raise Exception("source_uid does not match the source_uid in the database.")
+            if result["source_uid"] != data["source_uid"]:
+                raise Exception(
+                    "source_uid does not match the source_uid in the database."
+                )
 
         print(f"Verified information for {result['name']}")
 
@@ -51,8 +61,8 @@ class InsertDataService:
         #     )
         # ).fetchone()[0]
 
-    def addData(self):
-        self._verifyInformation()
+    def addData(self, data):
+        self._verifyInformation(data)
 
         # self._metadata_obj.reflect(self._engine)
         """
@@ -68,10 +78,14 @@ class InsertDataService:
         """
 
         table = sqlalchemy.Table(
-            self._source_uid,
+            data["source_uid"],
             self._metadata_obj,
             autoload=True,
             autoload_with=self._engine,
+            keep_existing=True,
         )
-        stmt = table.insert().values(**{self._metric_uid: self.data})
+        values = {}
+        for _metric in data["metrics"]:
+            values[_metric["metric_uid"]] = _metric["value"]
+        stmt = table.insert().values({**values, "timestamp": datetime.now()})
         self._connection.execute(stmt)

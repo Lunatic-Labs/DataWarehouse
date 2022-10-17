a66a6e6b3a71cc11aa1b8cc4732e42c3f627d027
= <=>
2022-10-17 13:14:47 -0500

    index.php now displays group, source, and metric tables with a little bit of styling.

--- index.php
+++ index.php
@@ -2,23 +2,54 @@
 <html>
 <head>
   <title>Source Database Information</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <?php
     $conn = pg_connect("host=localhost port=5432 dbname=data_warehouse user=postgres password=postgres");
-    $sql = "SELECT * FROM metric;";
+
+    $sql = "SELECT * FROM \"group\";";
+    echo "<h3>----------------------------------GROUP TABLE----------------------------------</h3>" . "<br>";
     $result = pg_query($conn, $sql);
     $resultCheck = pg_num_rows($result);
     if ($resultCheck > 0) {
       while ($row = pg_fetch_assoc($result)) {
-        echo "metric_uid: " . $row['metric_uid'] . "<br>";
-        echo "source_uid: " . $row['source_uid'] . "<br>";
-        echo "data_type: " . $row['data_type'] . "<br>";
-        echo "units: " . $row['units'] . "<br>";
-        echo "name: " . $row['name'] . "<br>";
-        echo "asc: " . $row['asc'] . "<br>";
+        echo "name: "           . $row['name']           . "<br>";
+        echo "location: "       . $row['location']       . "<br>";
+        echo "classification: " . $row['classification'] . "<br>";
+        echo "group_uid: "      . $row['group_uid']      . "<br>";
         echo "<br>";
       }
+    }
+
+    $sql = "SELECT * FROM source;";
+    echo "<h3>----------------------------------SOURCE TABLE----------------------------------</h3>" . "<br>";
+    $result = pg_query($conn, $sql);
+    $resultCheck = pg_num_rows($result);
+    if ($resultCheck > 0) {
+      while ($row = pg_fetch_assoc($result)) {
+        echo "source_uid: " . $row['source_uid'] . "<br>";
+        echo "name: "       . $row['name']       . "<br>";
+        echo "group_uid: "  . $row['group_uid']  . "<br>";
+        echo "tz_info: "    . $row['tz_info']    . "<br>";
+        echo "<br>";
+      }
+    }
+
+    $sql = "SELECT * FROM metric;";
+    echo "<h3>----------------------------------METRIC TABLE----------------------------------</h3>" . "<br>";
+    $result = pg_query($conn, $sql);
+    $resultCheck = pg_num_rows($result);
+    if ($resultCheck > 0) {
+        while ($row = pg_fetch_assoc($result)) {
+            echo "metric_uid: " . $row['metric_uid'] . "<br>";
+            echo "source_uid: " . $row['source_uid'] . "<br>";
+            echo "data_type: "  . $row['data_type']  . "<br>";
+            echo "units: "      . $row['units']      . "<br>";
+            echo "name: "       . $row['name']       . "<br>";
+            echo "asc: "        . $row['asc']        . "<br>";
+            echo "<br>";
+        }
     }
   ?>
 </body>

40bc38505ed672b7c85b6dd1b5c57b534c1cc84f
= <=>
2022-10-21 18:10:08 -0500

    Removed getData(). Added function descriptions.

--- datawarehouse/service/data_insert_service.py
+++ datawarehouse/service/data_insert_service.py
@@ -8,29 +8,13 @@ from datawarehouse.model import metric
 
 
 class InsertDataService(BaseService):
-    # def __init__(self, url, source_uid=None, metric_uid=None, data=None):
-    #     """
-    #     TODO: Connect using our existing connections.
-    #     """
-    #     self._engine = db.engine
-    #     self._connection = self._engine.connect()
-    #     self._metadata_obj = db.meta
-
-    """
-    TODO: Change this from reading a txt file to taking JSON.
-    """
-
-    # def getData(self, file):
-    #     with open(file, "r") as f:
-    #         self._source_uid = f.readline()
-    #         self._metric_uid = f.readline()
-    #         self.data = f.readline()
-    #         # Getting rid of the newline characters.
-    #         self._source_uid = self._source_uid.rstrip()
-    #         self._metric_uid = self._metric_uid.rstrip()
-    #         self.data = self.data.rstrip()
-
     def verifyInformation(self, data):
+        """
+        verifyInformation(self, data) -> Err
+        Verifies that the provided UUID's exist in the tables
+        and verifies that they match.
+        TODO: Type checking.
+        """
         if not self._checkUUIDFormat(data["source_uid"]):
             return "source_uid is not a valid uuid4", 400
         for metric_ in data["metrics"]:
@@ -56,18 +40,9 @@ class InsertDataService(BaseService):
         # ).fetchone()[0]
 
     def addData(self, data):
-
-        # self._metadata_obj.reflect(self._engine)
         """
-        NOTE: Found this function and may be useful.
-        This is from the sqlalchemy inline documentation.
-
-        Load all available table definitions from the database.
-        Automatically creates ``Table`` entries in this ``MetaData`` for any
-        table available in the database but not yet present in the
-        ``MetaData``.  May be called multiple times to pick up tables recently
-        added to the database, however no special action is taken if a table
-        in this ``MetaData`` no longer exists in the database.
+        addData(self, data) -> void.
+        Takes `data` JSON, parses and adds the value to the table.
         """
         table = self._get_table(data["source_uid"])
         values = {}

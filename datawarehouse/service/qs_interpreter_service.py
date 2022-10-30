b6169f66127326cae2ca1d082dc91857f53950c5
Zachary Haskins <zacharydhaskins@yahoo.com>
2022-10-30 15:44:52 -0500

    more progress on the statement generation.

--- datawarehouse/service/qs_interpreter_service.py
+++ datawarehouse/service/qs_interpreter_service.py
@@ -1,12 +1,14 @@
 import sqlalchemy
+
+
 class QueryStringInterpreter:
     # To add a new operation, put it here.
     OPERATIONS = {
         "lt": "<",
         "gt": ">",
         "eq": "=",
-        "limit": "lim",
-        "and": "AND"
+        "limit": "LIMIT",
+        "and": "AND",
     }
 
     def __init__(self, url, db_url, query=None, tablename=None):
@@ -15,10 +17,11 @@ class QueryStringInterpreter:
         self.query = query
         self.stmnt_tokens = []
         self.op_tokens = []
+        self.stmnt_tokens_sz = 0
+        self.op_tokens_sz = 0
         self.statement = ""
         self.engine = sqlalchemy.create_engine(db_url)
         self.connection = self.engine.connect()
-
 
     def parseUrl(self):
         slash_idx = -1
@@ -41,23 +44,25 @@ class QueryStringInterpreter:
     def createTokens(self):
         if self.query == None:
             raise Exception("No query has been given. Run _.parseUrl().")
+
         sz = len(self.query)
         i = 0
         j = 0
-        # We could gain some speed by using arrays with a size instead of heap-allocated strings.
         opbuf = ""
         sbuf = ""
+
+        # I am so, so sorry.
         while i < sz:
             if i < sz - 1 and self.query[i] == "_" and self.query[i + 1] == "_":
-                # i + 2 to put us past the `__`
-                j = i + 2
+                j = i + 2  # i + 2 to put us past the `__`
                 while self.query[j] != "=":
                     opbuf += self.query[j]
                     j += 1
                 self.op_tokens.append(self._determineOperation(opbuf))
-                # +2 for `__`. This will put us past the operation.
-                i += len(opbuf) + 2
+                self.op_tokens_sz += 1
+                i += len(opbuf) + 2  # +2 for `__`. This will put us past the operation.
                 self.stmnt_tokens.append(sbuf)
+                self.stmnt_tokens_sz += 1
                 opbuf = ""
                 sbuf = ""
                 # TODO: Make sure `i` is on top of `=`.
@@ -65,32 +70,33 @@ class QueryStringInterpreter:
             else:
                 sbuf += self.query[i]
             i += 1
+
         self.stmnt_tokens.append(sbuf)
-        op_sz = len(self.op_tokens)
-        stmnt_sz = len(self.stmnt_tokens)
-        if op_sz != stmnt_sz - 1:
+        self.stmnt_tokens_sz += 1
+
+        if self.op_tokens_sz != self.stmnt_tokens_sz - 1:  # Currently broken.
             raise Exception(
-                f"UNIMPLEMENTED: Operations size `{op_sz}` and statements size `{stmnt_sz}` are imbalanced. Operations size should be statements size - 1"
+                f"UNIMPLEMENTED: Operations size `{self.op_tokens_sz}` and statements size `{self.stmnt_tokens_sz}` are imbalanced. Operations size should be statements size - 1"
             )
 
     def verifyTokenIntegrity(self):
         # TODO: Verify each token to make sure they are valid.
-        table = sqlalchemy.Table(self.tablename, sqlalchemy.MetaData(), autoload=True, autoload_with=self.engine)
+        table = sqlalchemy.Table(
+            self.tablename,
+            sqlalchemy.MetaData(),
+            autoload=True,
+            autoload_with=self.engine,
+        )
         pass
 
-
     def generateStatement(self):
-        # for i in range(0, len(self.stmnt_tokens)):
-        #     if i == 0:
-        #         self.stmnt_tokens[i] = f"SELECT * FROM {self.tablename} WHERE {self.stmnt_tokens[i]}"
-        #     else:
-        #         self.stmnt_tokens[i] = f"{self.op_tokens[i - 1]} {self.stmnt_tokens[i]}"
-        # return a sqlalchemy query object
-        for i in range(0, len(self.stmnt_tokens)):
-            if i == 0:
-                self.statement += f"SELECT * FROM \"{self.tablename}\" WHERE {self.stmnt_tokens[i]}"
-            else:
-                self.statement += f" {self.op_tokens[i - 1]} {self.stmnt_tokens[i]}"
+        if self.stmnt_tokens_sz == 0:
+            raise Exception("No tokens have been generated. Run _.createTokens().")
+        self.statement += (
+            f'SELECT * FROM "{self.tablename}" WHERE {self.stmnt_tokens[0]}'
+        )
+        for i in range(1, self.stmnt_tokens_sz):
+            self.statement += f" {self.op_tokens[i - 1]} {self.stmnt_tokens[i]}"
         return self.statement
 
     def dump(self):
@@ -102,8 +108,8 @@ class QueryStringInterpreter:
         print(f"STATEMENT: {self.statement}")
 
 
-url = "https://www.datawarehouse.com/7ef719c9-59da-4262-a94b-6d9bb17c2f11?feet__lt=3__and=beds__gt=2"
-dburl = "postgresql://zdh:silverfish@localhost:5432/data_warehouse"
+url = "https://www.datawarehouse.com/7ef719c9-59da-4262-a94b-6d9bb17c2f11?feet__lt=3__and=people__gt=2"
+dburl = "postgresql://postgres:postgres@localhost:5432/data_warehouse"
 q = QueryStringInterpreter(url, dburl)
 q.parseUrl()
 q.createTokens()

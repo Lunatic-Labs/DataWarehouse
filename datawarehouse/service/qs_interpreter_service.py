3a2f522ab08c282519e5f1e9725e98b7d613c3a2
Zachary Haskins <zacharydhaskins@yahoo.com>
2022-10-29 19:47:52 -0500

    Started tokenizing.

--- datawarehouse/service/qs_interpreter_service.py
+++ datawarehouse/service/qs_interpreter_service.py
@@ -1,8 +1,10 @@
 class QueryStringInterpreter:
-    def __init__(self, url, query=None, tablename=None):
+    def __init__(self, url, query, tablename):
         self.url = url
         self.tablename = tablename
         self.query = query
+        self.stmnt_tokens = []
+        self.op_tokens = []
 
     def parseUrl(self):
         slash_idx = -1
@@ -18,8 +20,52 @@ class QueryStringInterpreter:
         self.tablename = self.url[slash_idx+1:question_idx]
         self.query = self.url[question_idx+1:]
 
-    def parseQuery(self):
-        if self.query == None or self.tablename == None:
+    def _check(self):
+        return not (self.query == None and self.tablename == None)
+
+    def _determineOperation(self, op):
+        if op == "lt":
+            return '<'
+        elif op == "gt":
+            return '>'
+        elif op == "eq":
+            return '='
+        else:
+            return "op not implemented"
+
+    def createTokens(self):
+        if not self._check():
+            raise Exception("No query or tablename has been given. Run _.parseUrl().")
+        start = -1
+        end = -1
+        sz = len(self.query)
+        i = 0
+        j = 0
+        opbuf = ""
+        # sbuf = ""
+        while i < sz:
+            if start == -1:
+                start = i
+            if end != -1 or i == sz-1:
+                self.stmnt_tokens.append(self.query[start:end])
+                start = -1
+                end = -1
+            if i < sz-1 and self.query[i] == '_' and self.query[i+1] == '_':
+                end = i
+                j = i + 2 # i + 2 to put us past the `__`
+                while self.query[j] != '=':
+                    opbuf += self.query[j]
+                    j += 1
+                self.op_tokens.append(self._determineOperation(opbuf))
+                i += len(opbuf) + 2 # +2 for `__`. This will put us past the operation.
+                buf = ""
+            i += 1
+
+        print(self.stmnt_tokens)
+        print(self.op_tokens)
+
+    def generateStatement(self):
+        if not self._check():
             raise Exception("No query or tablename has been given. Run _.parseUrl().")
 
     def dump(self):
@@ -27,12 +73,10 @@ class QueryStringInterpreter:
         print(f"tablename = {self.tablename}")
         print(f"query = {self.query}")
 
-
-
 qs = "https://www.datawarehouse.com/thermometertable?timestamp__lt=10pm10/30/22"
-q = QueryStringInterpreter(qs)
+q = QueryStringInterpreter(qs, None, None)
 q.parseUrl()
-q.parseQuery()
+q.createTokens()
 q.dump()
 
 

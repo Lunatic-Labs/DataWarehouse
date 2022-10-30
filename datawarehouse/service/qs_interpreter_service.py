46a0cbdf0ac99fc9d7b22b5eb39a65f481ef22c2
Zachary Haskins <zacharydhaskins@yahoo.com>
2022-10-29 20:26:27 -0500

    Added an easy way to add new operations.

--- datawarehouse/service/qs_interpreter_service.py
+++ datawarehouse/service/qs_interpreter_service.py
@@ -1,4 +1,11 @@
 class QueryStringInterpreter:
+    # To add a new operation, put it here.
+    OPERATIONS = {
+        "lt": '<',
+        "gt": '>',
+        "eq": '=',
+        "limit": "lim",
+    }
     def __init__(self, url, query, tablename):
         self.url = url
         self.tablename = tablename
@@ -9,14 +16,12 @@ class QueryStringInterpreter:
     def parseUrl(self):
         slash_idx = -1
         question_idx = -1
-
         for i in range(0, len(self.url)):
             if self.url[i] == '/':
                 slash_idx = i;
             if self.url[i] == '?':
                 question_idx = i
                 break
-
         self.tablename = self.url[slash_idx+1:question_idx]
         self.query = self.url[question_idx+1:]
 
@@ -24,14 +29,10 @@ class QueryStringInterpreter:
         return not (self.query == None and self.tablename == None)
 
     def _determineOperation(self, op):
-        if op == "lt":
-            return '<'
-        elif op == "gt":
-            return '>'
-        elif op == "eq":
-            return '='
-        else:
-            return "op not implemented"
+        for key in self.OPERATIONS:
+            if op == key:
+                return self.OPERATIONS[key]
+        return f"Operation `{op}` not implemented."
 
     def createTokens(self):
         if not self._check():
@@ -39,16 +40,19 @@ class QueryStringInterpreter:
         sz = len(self.query)
         i = 0
         j = 0
+        # We could gain some speed by using arrays with a size instead of heap-allocated strings.
         opbuf = ""
         sbuf = ""
         while i < sz:
             if i < sz-1 and self.query[i] == '_' and self.query[i+1] == '_':
-                j = i + 2 # i + 2 to put us past the `__`
+                # i + 2 to put us past the `__`
+                j = i + 2
                 while self.query[j] != '=':
                     opbuf += self.query[j]
                     j += 1
                 self.op_tokens.append(self._determineOperation(opbuf))
-                i += len(opbuf) + 2 # +2 for `__`. This will put us past the operation.
+                # +2 for `__`. This will put us past the operation.
+                i += len(opbuf) + 2
                 opbuf = ""
                 self.stmnt_tokens.append(sbuf)
                 sbuf = ""

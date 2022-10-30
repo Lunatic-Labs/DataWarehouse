fc1b2da0d43211494d31fcdb5b947bf17253e449
Zachary Haskins <zacharydhaskins@yahoo.com>
2022-10-30 16:54:13 -0500

    Some renaming and a lot of documentation.

--- datawarehouse/service/qs_interpreter_service.py
+++ datawarehouse/service/qs_interpreter_service.py
@@ -1,7 +1,58 @@
 import sqlalchemy
 
+"""
+QSInterpreterService Documentation:
+    I. Order of Operations:
+        a) Create a QSInterpreterService object
+        b) Call parseUrl() to parse the url
+        c) Call createTokens() to create tokens from the parsed url
+        d) Call verifyTokenIntegrity() to verify the integrity of the tokens
+        e) Call generateStatement() to generate a statement from the tokens
+    II. Overview of Methods:
+        a) parseUrl() -> void:
+            - Time Complexity: O(n), where n is the length of the url.
+            - Splits the url into two parts.
+                1. The first part is the tablename.
+                2. The second part is the query.
+        b) createTokens() -> void:
+            - Time Complexity: O(n^m), where n is the number of characters in the query and m is the length of the operation.
+            - Splits the query into two sets of tokens.
+                1. stmnt_tokens: These are non-conditional tokens.
+                2. op_tokens: These are conditional tokens.
+            - There must always be one more stmnt_token than op_token.
+        c) verifyTokenIntegrity() -> void:
+            - UNFINISHED.
+            - Time Complexity: O(n^m), where n is the number of stmnt_tokens and m is the number of ILLEGALS.
+            - Here we will make sure each token is valid.
+            - I have also added some illegal operations that cannot be in the tokens (not sure if this is necessary).
+        d) generateStatement() -> str:
+            - Time Complexity: O(n), where n is the number of stmnt_tokens.
+            - This method will construct a sql statement from the tokens.
+            - It starts with `SELECT * FROM` and then adds the tablename.
+            - Then it adds the stmnt_tokens and op_tokens.
+            - For every op_token, there is a corresponding stmnt_token on the left and right of it.
+            - Example:
+                -> Query: 7ef719c9-59da-4262-a94b-6d9bb17c2f11?elem1__lt=n__and=elem2__gt=m
+                -> Tablename: 7ef719c9-59da-4262-a94b-6d9bb17c2f11
+                -> stmnt_tokens: ['elem1', 'n', 'elem2', 'm']
+                -> op_tokens: ['<', 'AND', '>']
+                -> Statement: SELECT * FROM 7ef719c9-59da-4262-a94b-6d9bb17c2f11 WHERE elem1 < n AND elem2 > m
+            - You can also ommitt the query part of the url and it will select everything from the table.
+            - However, it must contain `?`.
+            - Example:
+                -> Query: 7ef719c9-59da-4262-a94b-6d9bb17c2f11?
+                -> Tablename: 7ef719c9-59da-4262-a94b-6d9bb17c2f11
+                -> stmnt_tokens: []
+                -> op_tokens: []
+                -> Statement: SELECT * FROM 7ef719c9-59da-4262-a94b-6d9bb17c2f11
+        e) dump() -> void:
+            - Time Complexity: O(1).
+            - This method will print the URL, tablename, stmnt_tokens, op_tokens, and statement.
+            - It is best to call this method at the end.
+"""
 
-class QueryStringInterpreter:
+
+class QSInterpreterService:
     # To add a new operation, put it here.
     OPERATIONS = {
         "lt": "<",
@@ -128,7 +179,7 @@ class QueryStringInterpreter:
 url = "https://www.datawarehouse.com/7ef719c9-59da-4262-a94b-6d9bb17c2f11?feet__lt=3__and=people__gt=2"
 # url = "https://www.datawarehouse.com/7ef719c9-59da-4262-a94b-6d9bb17c2f11?"
 dburl = "postgresql://postgres:postgres@localhost:5432/data_warehouse"
-q = QueryStringInterpreter(url, dburl)
+q = QSInterpreterService(url, dburl)
 q.parseUrl()
 q.createTokens()
 q.verifyTokenIntegrity()

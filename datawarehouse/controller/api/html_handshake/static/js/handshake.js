19571cafcdfcbcfae8cc61a699188d3909dc6464
nmullins417 <nmullins417@gmail.com>
2022-11-16 17:01:27 -0600

    Factored out some code into two new JS functions

--- datawarehouse/controller/api/html_handshake/static/js/handshake.js
+++ datawarehouse/controller/api/html_handshake/static/js/handshake.js
@@ -1,6 +1,30 @@
 //keeps track of the number of metrics belonging to each source (# sources = array length)
 var num_metrics = [1];
 
+//takes the ID of a div in the format "0.0" (a string) and slices/parses it to find the 
+//source and metric number (returns [0, 0] both ints)
+function findIndices(ID_name) {
+    var index = ID_name.indexOf(".");
+    var src = parseInt(ID_name.slice(0, index));
+    var metric = parseInt(ID_name.slice(index + 1));
+    return [src, metric];
+}
+
+// takes the outer div (the one that will hold all the info for a source) and creates an inner
+// div that will either hold (1) overview info about the source, or (2) info about the first metric 
+// defined for a source 
+// inner_div_template will pertain to either (1) overview info, or (2) source #1 info
+// ovr_or_metric: true -> create overview div; false -> create source #1 div
+function createInnerDivs(outer_div, inner_div_template, ovr_or_metric) {
+    var new_inner_div = document.createElement('div');
+    if (ovr_or_metric)
+        new_inner_div.id = (num_metrics.length - 1) + ".overview";
+    else
+        new_inner_div.id = (num_metrics.length - 1) + ".0";
+    new_inner_div.innerHTML = inner_div_template.innerHTML;
+    outer_div.appendChild(new_inner_div);
+}
+
 function addSource() {
     //each new source must have at least 2 elements--overview info and 
     //info for at least one metric. Copy these two items from the most recent
@@ -11,47 +35,38 @@ function addSource() {
     //extend array to account for new source
     num_metrics.push(1);
 
-    //create new source div that will hold the divs for the overview/metric info
+    //create new source (outer) div that will hold the divs for the overview/metric info
     var new_source = document.createElement('div');
     new_source.id = 'source' + (num_metrics.length - 1);
     document.getElementsByTagName('form')[0].appendChild(new_source);
 
-    //create div for overview info for new source, and put it inside the new source div
-    var new_source_overview = document.createElement('div');
-    new_source_overview.id = (num_metrics.length - 1) + ".overview";
-    new_source_overview.innerHTML = overview.innerHTML;
-    new_source.appendChild(new_source_overview);
-
-    //the same thing as above, but for the div containing info about the first new metric
-    var new_first_metric = document.createElement('div');
-    new_first_metric.id = (num_metrics.length - 1) + ".0";
-    new_first_metric.innerHTML = first_metric.innerHTML;
-    new_source.appendChild(new_first_metric);
+    //create new inner divs that will contain overview info and metric #1 info about the
+    // new source
+    createInnerDivs(new_source, overview, true);
+    createInnerDivs(new_source, first_metric, false);
 }
 
 function addMetric(element) {
-    //window.alert(element.id);
     //get name of div ID where the button was clicked
     var ID_name = element.parentNode.id;
 
     //get index of source for new div, so we can add it to the right section on the page
-    var index = ID_name.indexOf(".");
-    var src = parseInt(ID_name.slice(0, index));
+    var indices = findIndices(ID_name);
 
     //get contents of a previous metric info div to copy into the new one (acts as a template)
     var last_metric = document.getElementById("0.0");
 
     //create new metric info div; give it the correct ID name and copy the fields from the template
     var new_metric = document.createElement("div");
-    new_metric.id = src + "." + num_metrics[src];
+    new_metric.id = indices[0] + "." + num_metrics[indices[0]];
     new_metric.innerHTML = last_metric.innerHTML;
 
     //append the div to the correct source
-    var current_src = document.getElementById("source" + src);
+    var current_src = document.getElementById("source" + indices[0]);
     current_src.appendChild(new_metric)
 
     //the current source now has one more metric
-    num_metrics[src] += 1;
+    num_metrics[indices[0]] += 1;
 }
 
 function removeMetric(element) {
@@ -62,11 +77,10 @@ function removeMetric(element) {
     var div_name = document.getElementById(element.parentNode.parentNode.id);
 
     //get index of source for new div, so we can add it to the right section on the page
-    var index = ID_name.indexOf(".");
-    var src = parseInt(ID_name.slice(0, index));
+    var indices = findIndices(ID_name);
 
     //if only one metric defined, don't allow user to delete metric...
-    if (num_metrics[src] == 1) {
+    if (num_metrics[indices[0]] == 1) {
         window.alert("At least one metric must be defined for each source");
         return;
     }
@@ -74,14 +88,14 @@ function removeMetric(element) {
     //...if more than one metric defined, let user delete the source...
     var div_id = document.getElementById(ID_name);
     div_id.remove();
-    num_metrics[src] -= 1;
+    num_metrics[indices[0]] -= 1;
 
     //...then update the inner div names so there are no gaps (ex. _.0 is 1st, _.1 is 2nd, etc.)
     //get list of inner div names; 1st element is "_.overview"
     var metric_names = div_name.getElementsByTagName('div');
     var i, j;
     for (i = 1, j = 0; i < metric_names.length; i++, j++)
-        metric_names[i].id = `${src}.${j}`;
+        metric_names[i].id = `${indices[0]}.${j}`;
 }
 
 //get list of html elements to change to "Source #1", "Source #2", etc.
@@ -102,12 +116,10 @@ document.addEventListener("click", function () {
         //get ID of div ("0.0", "1.2", etc.)
         var ID_name = metric_titles[i].parentNode.id;
 
-        //find source and metric number of ID_name by slicing the string
-        var index = ID_name.indexOf(".");
-        var src = parseInt(ID_name.slice(0, index));
-        var metric = parseInt(ID_name.slice(index + 1));
+        //get source/metric numbers of the div ID
+        var indices = findIndices(ID_name);
 
         //change the heading
-        metric_titles[i].innerHTML = `Metric #${metric + 1} for Source #${src + 1}`;
+        metric_titles[i].innerHTML = `Metric #${indices[1] + 1} for Source #${indices[0] + 1}`;
     }
 });

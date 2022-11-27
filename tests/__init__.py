4bd927c9fa30e821fba7f3a5c35dd0e7a9ee2213
Taylor Hudson <taylorhudson42@gmail.com>
2022-11-27 08:20:13 -0600

    wrote simple tests for the query service

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,16 @@ from datawarehouse import create_app
 
 
 @pytest.fixture(scope="session")
+def session():
+    from datawarehouse.config.db import config
+    yield config.session
+
+@pytest.fixture(scope='session')
+def engine():
+    from datawarehouse.config.db import config 
+    yield config.engine
+
+@pytest.fixture(scope="session")
 def test_app():
     app = create_app(config="TestingConfig")
     return app.test_client()

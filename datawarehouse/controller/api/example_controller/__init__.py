60e15b23d693d9ce7fc55c2751aab464e907a682
Taylor Hudson <taylorhudson42@gmail.com>
2022-12-01 13:57:14 -0600

    removed unused files

--- datawarehouse/controller/api/example_controller/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-example_bp = Blueprint("example_bp", __name__, url_prefix="/datadogs")
-
-
-@example_bp.route("/", methods=["GET", "POST"])
-def get_request():
-    return "Hello Worlds ashdfasdfga"

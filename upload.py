e2dd2e01da38421970c27b8dec9d0736db9cd7f6
Justin T. Alexander <102481480+MrStarman18@users.noreply.github.com>
2023-02-12 16:45:24 -0600

    _postToHttps() and _encodeData() defined
    Not tested (not sure how to do that), but according to my sources they *should* work now. Planning to start on a function to pull data next.

--- upload.py
+++ upload.py
@@ -1,6 +1,7 @@
 import pycurl
 import certifi
 import urllib
+import urlparse
 
 import pycurl
 try:
@@ -11,7 +12,7 @@ except ImportError:
     from urllib import urlencode
 
 #code from http://pycurl.io/docs/latest/quickstart.html, I'm learning what it all does -Justin T.
-#post a given file to a given url using pycurl
+#post a given file to a given url using pycurl - url format should be 'https://website.com/' (including single quotes)
 def _postToHttps(url, filename):
     c = pycurl.Curl()
     c.setopt(c.URL, url)
@@ -23,26 +24,29 @@ def _postToHttps(url, filename):
     )),
     ])
 
-    #{field : value} needs to be filled in
+    #{field : value} needs to be filled in. How?
     post_data = {'field': 'value'}
-    # Form data must be provided already urlencoded. NEED TO DEFINE? IS FUNCTION DEFINED?
+    # Form data must be provided already urlencoded. Function defined here: https://docs.python.org/2/library/urllib.html#urllib.urlencode 
     postfields = urlencode(post_data)
     # Sets request method to POST,
-    # Content-Type header to application/x-www-form-urlencoded
+    # Content-Type header to application/x-www-form-urlencoded (the data type expected of string arguments)
     # and data to send in request body.
     c.setopt(c.POSTFIELDS, postfields)
 
     c.perform()
     c.close()
 
-#code from https://stackoverflow.com/questions/24572210/what-is-the-equivalent-to-curls-data-urlencode-in-pycurl
-#Figuring out what each part does, but may need to call this before post_data in above function.
-def _encodeData(url)
-    your_dict = {"a": "a", "b": "b"}
+#below: code from https://stackoverflow.com/questions/24572210/what-is-the-equivalent-to-curls-data-urlencode-in-pycurl
+
+def _encodeData(url, queryString):
+    your_dict = {urlparse.parse_qs(queryString)}
     res = urllib.request.urlopen(url), urllib.parse.urlencode(your_dict).encode()
     # Get the result
     return(res.read())
 
-# urlencode() defined here: https://docs.python.org/2/library/urllib.html#urllib.urlencode 
-# compare to urlparse.parse_qs() at: https://docs.python.org/2/library/urlparse.html#module-urlparse
-# parse seems to have options for converting query strings into strings/dictionaries. Is this what we need?
+# urlparse.parse_qs() defined here: https://docs.python.org/2/library/urlparse.html#module-urlparse
+# parse_qs() returns a dictionary made from the string argument (the query string)
+# If we need the string as name & value data pairs we should use parse_qsl()
+
+def _pullFromHttps():
+    #Going to define this function next.

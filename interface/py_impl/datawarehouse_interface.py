8b405e9d5689802fa965c83908b26df7725cdb8c
jdkingsbury <jerry.daniel.kingsbury@gmail.com>
2023-03-18 01:07:57 -0500

    Current progress. Not sure if parameters are correct or if the function needs to ddo more yet.

--- interface/py_impl/datawarehouse_interface.py
+++ interface/py_impl/datawarehouse_interface.py
@@ -1,4 +1,5 @@
 import os
+
 # import urllib       # This is most likely not needed, but idk.
 # import urlparse     # This is most likely not needed, but idk.
 # import urllib.parse # This is most likely not needed, but idk.
@@ -9,19 +10,29 @@ import uuid
 
 # Pycurl Documentation: http://pycurl.io/docs/latest/quickstart.html
 
+
 class DWInterface:
-    remote_ip_address = '3.216.190.202'
-    local_ip_address = '127.0.0.1'
+    remote_ip_address = "3.216.190.202"
+    local_ip_address = "127.0.0.1"
 
-    handshake_path = '/api/prepare/'
-    insert_path = '/api/store/'
-    query_path = '/api/query/'
+    handshake_path = "/api/prepare/"
+    insert_path = "/api/store/"
+    query_path = "/api/query/"
 
-    development_port = ':5000'
-    staging_port = ':4000'
-    production_port = ':3000'
+    development_port = ":5000"
+    staging_port = ":4000"
+    production_port = ":3000"
 
-    def __init__(self, username, password, ip=local_ip_address, port=development_port, group_uuid=None, source_uuid=None, metric_uuid=None):
+    def __init__(
+        self,
+        username,
+        password,
+        ip=local_ip_address,
+        port=development_port,
+        group_uuid=None,
+        source_uuid=None,
+        metric_uuid=None,
+    ):
         self.__username = username
         self.__password = password
         self.__ip = ip
@@ -29,7 +40,7 @@ class DWInterface:
         self.__group_uuid = group_uuid
         self.__source_uuid = source_uuid
         self.__metric_uuid = metric_uuid
-        self.__authority = 'http://' + ip + port
+        self.__authority = "http://" + ip + port
         self.__handshake_url = self.__authority + self.handshake_path
         self.__insert_url = self.__authority + self.insert_path
         self.__query_url = self.__authority + self.query_path
@@ -38,14 +49,14 @@ class DWInterface:
     # Private Functions.
 
     def __POSTRequest(self, url, json_filepath, out_filepath):
-        '''
+        """
         This function should not return anything.
         curl_handle.setopt(pycurl.HTTPPOST, [('fileupload', (pycurl.FORM_FILE, json_file)), ('string', 'string_value')])
-        '''
+        """
         outfp = None
 
         # Open the file if it is specified.
-        if (out_filepath != None):
+        if out_filepath != None:
             outfp = open(out_filepath, "w")
 
         # Create a buffer to recieve data.
@@ -54,39 +65,53 @@ class DWInterface:
         # Create CURL handle.
         self.__curl_handle = pycurl.Curl()
         self.__curl_handle.setopt(self.__curl_handle.URL, url)
-        self.__curl_handle.setopt(self.__curl_handle.HTTPPOST, [('fileupload', (self.__curl_handle.FORM_FILE, json_filepath))])
+        self.__curl_handle.setopt(
+            self.__curl_handle.HTTPPOST,
+            [("fileupload", (self.__curl_handle.FORM_FILE, json_filepath))],
+        )
         self.__curl_handle.perform()
         self.__curl_handle.close()
 
         # Get the response from the buffer and decode.
         body = buf.getvalue()
-        response = body.decode('iso-8859-1')
+        response = body.decode("iso-8859-1")
 
         # Write to a file if an outfile is specified.
-        if (out_filepath != None):
+        if out_filepath != None:
             outfp.write(response)
 
         return response
 
     def __GETRequest(self, query_string, out_filepath):
-        '''
+        """
         This function should return a string that is the response from the server.
-        '''
+        """
         outfp = None
 
         # Check to make sure the UUIDs have been set.
-        if (self.__group_uuid is None or self.__source_uuid is None or self.__metric_uuid is None):
-            raise ValueError('UUIDs must be set')
+        if (
+            self.__group_uuid is None
+            or self.__source_uuid is None
+            or self.__metric_uuid is None
+        ):
+            raise ValueError("UUIDs must be set")
 
-        if (query_string[0] != '?'):
-            raise ValueError('Query string must start with `?`')
+        if query_string[0] != "?":
+            raise ValueError("Query string must start with `?`")
 
         # Open the file if it is specified.
-        if (out_filepath != None):
+        if out_filepath != None:
             outfp = open(out_filepath, "w")
 
         # Create the url. It should be: 'http://ip_addr:port/group_uuid/source_uuid/query_string
-        url = self.__query_url + self.__group_uuid + '/' + self.__source_uuid + '/' + query_string
+        url = (
+            self.__query_url
+            + self.__group_uuid
+            + "/"
+            + self.__source_uuid
+            + "/"
+            + query_string
+        )
 
         # Create a buffer to recieve data.
         buf = BytesIO()
@@ -103,10 +128,10 @@ class DWInterface:
 
         # Get the response from the buffer and decode.
         body = buf.getvalue()
-        response = body.decode('iso-8859-1')
+        response = body.decode("iso-8859-1")
 
         # Write to a file if an outfile is specified.
-        if (out_filepath != None):
+        if out_filepath != None:
             outfp.write(response)
 
         return response
@@ -116,12 +141,20 @@ class DWInterface:
             uuid.UUID(str(unverified_uuid))
             return True
         except ValueError:
-            return False;
+            return False
 
     # Public Functions.
 
     def commitHandshake(self, handshake_json):
-        pass
+        infp = open(handshake_json, "r")
+        url = self.__handshake_url
+
+        # Perform Post request. (Not sure if self is the dwi parameter in c version)
+        response = self.__POSTRequest(self, url, infp)
+
+        infp.close()
+
+        return response
 
     def insertData(self, insert_json):
         pass
@@ -130,24 +163,29 @@ class DWInterface:
         return self.__GETRequest(query_string, out_file)
 
     def setUUIDs(self, group_uuid, source_uuid, metric_uuid):
-        if self.__verifyUUID(group_uuid) and self.__verifyUUID(source_uuid) and self.__verifyUUID(metric_uuid):
+        if (
+            self.__verifyUUID(group_uuid)
+            and self.__verifyUUID(source_uuid)
+            and self.__verifyUUID(metric_uuid)
+        ):
             self.__group_uuid = group_uuid
             self.__source_uuid = source_uuid
             self.__metric_uuid = metric_uuid
         else:
-            raise ValueError('Invalid UUIDs provided')
+            raise ValueError("Invalid UUIDs provided")
 
-if __name__ == '__main__': # Use this for running code, testing, debugging, etc.
-    guuid = 'c85ad1d4-2147-44eb-ba30-3206f26d6569'
-    suuid = '3d276782-6656-4e5e-b41c-6236ac86a021'
-    muuid = '75b9d8f7-9e39-47b5-909a-76d0e2c9cb13'
 
-    dw = DWInterface('usr', 'pass')
+if __name__ == "__main__":  # Use this for running code, testing, debugging, etc.
+    guuid = "c85ad1d4-2147-44eb-ba30-3206f26d6569"
+    suuid = "3d276782-6656-4e5e-b41c-6236ac86a021"
+    muuid = "75b9d8f7-9e39-47b5-909a-76d0e2c9cb13"
+
+    dw = DWInterface("usr", "pass")
     dw.setUUIDs(guuid, suuid, muuid)
 
 # Good job with this, it'll be useful.
-#post_request() is called within commit_handshake() and insert_data().
-#This seems to be where the gruntwork of POSTing using PycURL happens.
+# post_request() is called within commit_handshake() and insert_data().
+# This seems to be where the gruntwork of POSTing using PycURL happens.
 # post_request(dwi, url, json_file)
 # {
 #     c = pycurl.Curl()

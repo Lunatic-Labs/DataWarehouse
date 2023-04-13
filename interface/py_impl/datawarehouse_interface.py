5694593cfe6e9b7e25ac74881a42be4733d0ff4b
Justin T. Alexander <therealjalex38@gmail.com>
2023-04-13 14:35:51 -0500

    Working on jsonToString(). Framework laid, need to work with UUIDs.

--- interface/py_impl/datawarehouse_interface.py
+++ interface/py_impl/datawarehouse_interface.py
@@ -11,39 +11,43 @@ import json
 
 # Pycurl Documentation: http://pycurl.io/docs/latest/quickstart.html
 
-#Define the Data Warehouse data hierarchy: DWInterface > Groups > Sources > Metrics
+#Data hierarchy (DWI > G > S > M). 
+# Unlike their first implementation, using solely to store names and UUIDs.
+#These are used in the setVal() function later.
 class Metric:
     def __init__(
         self,
-        asc,
-        datatype,
         name,
-        units
+        value=None,
+        uuid=None
     ):
-        self.asc = asc
-        self.datatype = datatype
         self.name = name
-        self.units = units
+        self.uuid = uuid
+        self.value = value
 
 class Source:
     def __init__(
         self,
         name,
-        metric=None
+        metric=None,
+        uuid=None
     ):
         self.name = name
         self.metrics = list(metric)
+        self.uuid = uuid
 
 class Group:
     def  __init__(
         self,
         classification,
         name,
-        source=None
+        source=None,
+        uuid=None
     ):
         self.classification = classification
         self.name = name
         self.sources = list(source)
+        self.uuid = uuid
 
 class DWInterface:
     remote_ip_address = "3.216.190.202"
@@ -52,6 +56,10 @@ class DWInterface:
     handshake_path = "/api/prepare/"
     insert_path = "/api/store/"
     query_path = "/api/query/"
+
+    interface_handshake_path = "/api/interface_prepare/"
+    interface_insert_path = "/api/interface_store/"
+    interface_query_path = "/api/interface_query/" 
 
     development_port = ":5000"
     staging_port = ":4000"
@@ -65,7 +73,7 @@ class DWInterface:
         port=development_port,
         group_uuid=None,
         source_uuid=None,
-        metric_uuid=None
+        metric_uuid=None,
     ):
         
         self.__username = username
@@ -79,12 +87,15 @@ class DWInterface:
         self.__handshake_url = self.__authority + self.handshake_path
         self.__insert_url = self.__authority + self.insert_path
         self.__query_url = self.__authority + self.query_path
+        self.__interface_handshake_url = self.__authority + self.interface_handshake_path
+        self.__interface_insert_url = self.__authority + self.interface_insert_path
+        self.__interface_query_url = self.__authority + self.interface_query_path
         self.__curl_handle = pycurl.Curl()
         self.groups = list()
 
     # Private Functions.
 
-    def __POSTRequest(self, json_filepath, out_filepath):
+    def __POSTRequest(self, json_filepath, out_filepath, url):
         """
         This function should return the data from commitHandshake()
         as there is no need to return anything from insertData().
@@ -103,7 +114,7 @@ class DWInterface:
         # Create a buffer to recieve data.
         buf = BytesIO()
 
-        self.__curl_handle.setopt(self.__curl_handle.URL, self.__handshake_url)
+        self.__curl_handle.setopt(self.__curl_handle.URL, url) #Should use interface_handshake_url?
         self.__curl_handle.setopt(pycurl.HTTPHEADER, ['Content-Type: application/json'])
         self.__curl_handle.setopt(self.__curl_handle.POSTFIELDS, json.dumps(json_file))
         self.__curl_handle.setopt(self.__curl_handle.WRITEFUNCTION, lambda x: None)
@@ -141,7 +152,7 @@ class DWInterface:
 
         # Create the url. It should be: 'http://ip_addr:port/group_uuid/source_uuid/query_string
         url = (
-            self.__query_url
+            self.__query_url    #Should be interface_query_url?
             + self.__group_uuid
             + "/"
             + self.__source_uuid
@@ -182,10 +193,10 @@ class DWInterface:
     # Public Functions.
 
     def commitHandshake(self, handshake_json, out_file_path=None):
-        return self.__POSTRequest(handshake_json, out_file_path)
+        return self.__POSTRequest(handshake_json, out_file_path, self.__handshake_url)
 
     def insertData(self, insert_json):
-        self.__POSTRequest(insert_json, None)
+        self.__POSTRequest(insert_json, None, self.__insert_url)
 
     def queryData(self, query_string, out_file=None):
         return self.__GETRequest(query_string, out_file)
@@ -199,27 +210,66 @@ class DWInterface:
             self.__group_uuid = group_uuid
             self.__source_uuid = source_uuid
             self.__metric_uuid = metric_uuid
+            #Set each UUID in related Group, Source, and Metric classes too?
         else:
             raise ValueError("Invalid UUIDs provided")
-        
-    def dwInterfaceGroupCreate(self, classification, name):
-        self.groups.append(Group(classification, name))
+    
+    def jsonToString(self, source, metric, handshake_filepath):
+        #Receive Source and Metric by plaintext name
 
-    def dwInterfaceSourceCreate(self, group, name):
-        group.sources.append(Source(name))
+        #Find related UUIDs for both
+        suuid = None
+        muuid = None
+        with open(handshake_filepath, "r") as file:
+            lines = file.readlines()
+            for row in lines:
+                if source in row:
+                    #save source_uuid
+                    suuid = "find the UUID here"
+                    #look within source for metric
+                    temp = "replace with lines contained in source"
+                    for row2 in temp:
+                        if metric in row2:
+                            muuid = "find the UUID here"
 
-    def dwInterfaceMetricCreate(self, source, name, units):
-        source.metrics.append(Metric(name, units))
+                            break #after finding Metric. No need to keep looking through Source.    
+                    break #after finding Source. No need to keep looking through file.
 
-    def checkLength(self, thing):       #Returns the length of of a DWInterface, Group, or Source 
-        if (type(thing) == DWInterface):
-            return len(thing.groups)
-        elif (type(thing) == Group):
-            return len(thing.sources)
-        elif (type(thing) == Source):
-            return len(thing.metrics)
-        else
-            return
+        if suuid == None:
+            raise ValueError("Source not in provided handshake file")
+        elif muuid == None:
+            raise ValueError("Metric not in provided handshake file")
+
+        #Write relevant data as string (":"-separated) and return 
+
+    def setVal(self, source, metric, value, handshake_filepath):    #Function concept using nested classes for hierarchy, may not be used
+        #Receive Source and Metric by plaintext name
+        #Find related UUIDs for both. Need to have classes saved for this?
+        source_uuid = ""
+        metric_uuid = ""
+        for i in self.groups:
+            for j in i.sources:
+                if j.name.lower() == source.lower():
+                    source_uuid = j.uuid    #placeholder line, NEED TO: store UUID in Source?
+                    source = j
+        for i in source.metrics:
+            if i.name.lower() == metric.lower():
+                metric_uuid = i.uuid        #placeholder 2, NEED TO: store UUID in Metric?
+                metric = j
+
+        #If UUIDs were found properly
+        if (
+            source_uuid != "" 
+            and metric_uuid != ""
+            and self.__verifyUUID(source_uuid)
+            and self.__verifyUUID(metric_uuid)
+        ):
+            metric.value = value
+            #NEED TO: Set the value at Metric (the found UUID, in the warehouse) to provided value
+        else:
+            raise ValueError("Could not find UUIDs based on provided names")
+            
+
 
 
 if __name__ == "__main__":  # Use this for running code, testing, debugging, etc.
@@ -232,6 +282,7 @@ if __name__ == "__main__":  # Use this for running code, testing, debugging, etc
     dw.setUUIDs(guuid, suuid, muuid)
     #dw.insertData("../../../insert.json")
     dw.insertData("insert.json")
-
+    #Below: Example call to setVal function
+    dw.setVal("Python Class Stats", "students_present", 50, "handshake.out")
 
 
